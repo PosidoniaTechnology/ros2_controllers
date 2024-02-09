1f57768cf0270ff26d74e170ce4895a43fa366c4
Nibanovic <nibanovic@gmail.com>
2024-02-09 15:36:42 +0000

    wrote tests for controller lifecycle

--- path_following_controller/test/test_path_following_controller.cpp
+++ path_following_controller/test/test_path_following_controller.cpp
@@ -14,12 +14,134 @@
 #include <utility>
 #include <vector>
 
+#include "lifecycle_msgs/msg/state.hpp"
+
+using lifecycle_msgs::msg::State;
+
 using namespace test_path_following_controller;
 
-TEST_F(FixturePFC, test_setup)
+class ParametrizedFixturePFC
+: public FixturePFC,
+  public ::testing::WithParamInterface<
+    std::tuple<std::vector<std::string>, std::vector<std::string>>>
 {
-  ASSERT_EQ(1,1);
+public:
+  virtual void SetUp()
+  {
+    FixturePFC::SetUp();
+    command_interface_types_ = std::get<0>(GetParam());
+    state_interface_types_ = std::get<1>(GetParam());
+  }
+};
+
+TEST_P(ParametrizedFixturePFC, can_configure)
+{
+  rclcpp::executors::MultiThreadedExecutor ex;
+  InitializePFC(ex);
+  auto state = ConfigurePFC().id();
+  ASSERT_EQ(state, State::PRIMARY_STATE_INACTIVE);
 }
+
+TEST_P(ParametrizedFixturePFC, can_activate)
+{
+  rclcpp::executors::MultiThreadedExecutor ex;
+  InitializePFC(ex);
+  ConfigurePFC();
+  auto state = ActivatePFC().id();
+  ASSERT_EQ(state, State::PRIMARY_STATE_ACTIVE);
+}
+
+TEST_P(ParametrizedFixturePFC, can_update_and_deactivate)
+{
+  rclcpp::executors::MultiThreadedExecutor ex;
+  InitializeConfigureActivatePFC(ex);
+  UpdatePFC(rclcpp::Duration::from_seconds(0.5));
+
+  auto state = DeactivatePFC().id();
+  ASSERT_EQ(state, State::PRIMARY_STATE_INACTIVE);
+}
+
+TEST_P(ParametrizedFixturePFC, can_clean_up)
+{
+  rclcpp::executors::MultiThreadedExecutor ex;
+  InitializeConfigureActivatePFC(ex);
+  UpdatePFC(rclcpp::Duration::from_seconds(0.5));
+  DeactivatePFC();
+
+  auto state = CleanUpPFC().id();
+  ASSERT_EQ(state, State::PRIMARY_STATE_UNCONFIGURED);
+}
+
+
+TEST_P(ParametrizedFixturePFC, configured_without_activation_ignores_commands)
+{
+  rclcpp::executors::MultiThreadedExecutor ex;
+
+  InitializePFC(ex);  
+  ConfigurePFC();
+
+  PublishToJointTrajectory(DEFAULT_DELAY_BETWEEN_POINTS,
+                           THREE_POINTS_POS, 
+                           THREE_POINTS_VEL);
+
+  controller_->wait_for_trajectory(ex);
+
+  UpdatePFC(rclcpp::Duration::from_seconds(0.5));
+
+  // hw position == 0 because controller is not activated
+  EXPECT_EQ(0.0, joint_pos_[0]);
+  EXPECT_EQ(0.0, joint_pos_[1]);
+  EXPECT_EQ(0.0, joint_pos_[2]);
+}
+
+TEST_P(ParametrizedFixturePFC, can_clean_up_after_configure)
+{
+  rclcpp::executors::MultiThreadedExecutor ex;
+  InitializePFC(ex),
+  ConfigurePFC();
+
+  auto state = CleanUpPFC().id();
+  ASSERT_EQ(state, State::PRIMARY_STATE_UNCONFIGURED);
+}
+
+TEST_P(ParametrizedFixturePFC, controller_holds_on_activation)
+{
+  rclcpp::executors::MultiThreadedExecutor ex;
+  InitializeConfigureActivatePFC(ex);
+  UpdatePFC(rclcpp::Duration::from_seconds(0.5));
+  
+  EXPECT_EQ(INITIAL_POS_JOINT1, joint_pos_[0]);
+  EXPECT_EQ(INITIAL_POS_JOINT2, joint_pos_[1]);
+  EXPECT_EQ(INITIAL_POS_JOINT3, joint_pos_[2]);
+  EXPECT_TRUE(controller_->is_holding());
+}
+
+TEST_P(ParametrizedFixturePFC, can_reactivate_from_parameters)
+{
+  rclcpp::executors::MultiThreadedExecutor ex;
+  InitializeConfigureActivatePFC(ex);
+
+  PublishToJointTrajectory(DEFAULT_DELAY_BETWEEN_POINTS,
+                            THREE_POINTS_POS, 
+                            THREE_POINTS_VEL);
+
+  // think about how to run one cycle, process one point,
+  // deactivate and then reactivate.
+
+  EXPECT_TRUE(controller_->is_holding());
+}
+
+
+
+using namespace std;
+INSTANTIATE_TEST_SUITE_P(
+  PositionControl, ParametrizedFixturePFC,
+  ::testing::Values(
+    make_tuple(vector<string>({POSITION}), vector<string>({POSITION}))//,
+    //make_tuple(vector<string>({POSITION}), vector<string>({POSITION, VELOCITY})),
+    //make_tuple(vector<string>({POSITION}), vector<string>({POSITION, VELOCITY, ACCELERATION}))
+    )
+  );
 
 int main(int argc, char ** argv)
 {
--- path_following_controller/test/test_path_following_controller.hpp
+++ path_following_controller/test/test_path_following_controller.hpp
@@ -37,6 +37,7 @@ const std::string POSITION = "position";
 const std::string VELOCITY = "velocity";
 const std::string ACCELERATION = "acceleration";
 const std::string EFFORT = "effort";
+
 const double INITIAL_POS_JOINT1 = 1.1;
 const double INITIAL_POS_JOINT2 = 2.1;
 const double INITIAL_POS_JOINT3 = 3.1;
@@ -45,6 +46,15 @@ const std::vector<double> INITIAL_POS_JOINTS = {
 const std::vector<double> INITIAL_VEL_JOINTS = {0.0, 0.0, 0.0};
 const std::vector<double> INITIAL_ACC_JOINTS = {0.0, 0.0, 0.0};
 const std::vector<double> INITIAL_EFF_JOINTS = {0.0, 0.0, 0.0};
+
+const std::vector<std::vector<double>> THREE_POINTS_POS = {
+  {{3.3, 4.4, 5.5}}, {{7.7, 8.8, 9.9}}, {{10.10, 11.11, 12.12}}
+};
+const std::vector<std::vector<double>> THREE_POINTS_VEL = {
+  {{0.01, 0.01, 0.01}}, {{0.05, 0.05, 0.05}}, {{0.06, 0.06, 0.06}}
+};
+
+const auto DEFAULT_DELAY_BETWEEN_POINTS = rclcpp::Duration(std::chrono::milliseconds(250));
 
 }  // namespace
 
@@ -65,6 +75,7 @@ public:
     }
     return ret;
   }
+
   /**
    * @brief wait_for_trajectory block until a new JointTrajectory is received.
    * Requires that the executor is not spinned elsewhere between the
@@ -112,6 +123,8 @@ public:
 
   path_following_controller::SegmentTolerances get_tolerances() const
     { return default_tolerances_; }
+  
+  void trigger_declare_parameters() { param_listener_->declare_params(); }
 
   JointTrajectoryPoint get_state_feedback() { return state_current_; }
   JointTrajectoryPoint get_state_reference() { return state_desired_; }
@@ -129,18 +142,16 @@ public:
     return has_active_trajectory() && traj_external_point_ptr_->has_nontrivial_msg();
   }
 
+  bool is_holding() { return *(rt_is_holding_.readFromRT()); }
+
   rclcpp::WaitSet subscriber_wait_set_;
 };
 
 class FixturePFC : public testing::Test
 {
 public:
-  //wrapped:
-  // on_configure + assign wait set to subscriber
-  // exposes private members and functions.
-  virtual void SetUp() override {
-    std::cout << "setting up fixture" << std::endl;
 
+  virtual void SetUp() override {
     controller_name_ = "test_path_following_controller";
     joint_names_ = {"joint1", "joint2", "joint3"};
     joint_pos_.resize(joint_names_.size(), 0.0);
@@ -151,20 +162,273 @@ public:
     joint_state_acc_.resize(joint_names_.size(), 0.0);
     joint_eff_.resize(joint_names_.size(), 0.0);
 
-    // Default interface values - they will be overwritten by parameterized tests
-    command_interface_types_ = {"position"};
-    state_interface_types_ = {"position"};
+    // Default interface values - they will be overwritten in parameterized tests
+    command_interface_types_ = {POSITION};
+    state_interface_types_ = {VELOCITY};
+ 
+    node_ = std::make_shared<rclcpp::Node>("trajectory_publisher_");
+    trajectory_publisher_ = node_->create_publisher<trajectory_msgs::msg::JointTrajectory>(
+      controller_name_ + "/joint_trajectory", rclcpp::SystemDefaultsQoS());
   }
 
-  virtual void TearDown() override {
-    std::cout << "tearing down fixture" << std::endl;
+  virtual void TearDown() override {}
+
+  void InitializePFC(
+    rclcpp::Executor & executor,
+    const std::vector<rclcpp::Parameter> & parameters = {})
+  {
+    controller_ = std::make_shared<TestablePathFollowingController>();
+
+    std::vector<rclcpp::Parameter> parameter_overrides;
+    parameter_overrides.push_back(rclcpp::Parameter("joints", joint_names_));
+    parameter_overrides.push_back(
+      rclcpp::Parameter("command_interfaces", command_interface_types_));
+    parameter_overrides.push_back(rclcpp::Parameter("state_interfaces", state_interface_types_));
+    parameter_overrides.push_back(
+      rclcpp::Parameter("allow_nonzero_velocity_at_trajectory_end", true));
+    parameter_overrides.insert(parameter_overrides.end(), parameters.begin(), parameters.end());
+
+    auto node_options = rclcpp::NodeOptions();
+    node_options.parameter_overrides(parameter_overrides);
+
+    auto ret = controller_->init(controller_name_, "", node_options);
+
+    if (ret != controller_interface::return_type::OK)
+    {
+      FAIL();
+    }
+    executor.add_node(controller_->get_node()->get_node_base_interface());
   }
 
-  virtual void LoadPFCParameters(){};
-  virtual void InitializePFC(){};
-  virtual void ConfigurePFC(){};
-  virtual void ActivatePFC(){};
-  virtual void SetUpAndActivatePFC(){};
+  rclcpp_lifecycle::State ConfigurePFC(){
+    return controller_->configure();
+  }
+      
+  rclcpp_lifecycle::State ActivatePFC(
+    bool separate_cmd_and_state_values = false,
+    const std::vector<double> initial_pos_joints = INITIAL_POS_JOINTS,
+    const std::vector<double> initial_vel_joints = INITIAL_VEL_JOINTS,
+    const std::vector<double> initial_acc_joints = INITIAL_ACC_JOINTS,
+    const std::vector<double> initial_eff_joints = INITIAL_EFF_JOINTS)
+  {
+    std::vector<hardware_interface::LoanedCommandInterface> cmd_interfaces;
+    std::vector<hardware_interface::LoanedStateInterface> state_interfaces;
+    pos_cmd_interfaces_.reserve(joint_names_.size());
+    vel_cmd_interfaces_.reserve(joint_names_.size());
+    acc_cmd_interfaces_.reserve(joint_names_.size());
+    eff_cmd_interfaces_.reserve(joint_names_.size());
+    pos_state_interfaces_.reserve(joint_names_.size());
+    vel_state_interfaces_.reserve(joint_names_.size());
+    acc_state_interfaces_.reserve(joint_names_.size());
+
+    for (size_t i = 0; i < joint_names_.size(); ++i)
+    {
+      pos_cmd_interfaces_.emplace_back(hardware_interface::CommandInterface(
+        joint_names_[i], hardware_interface::HW_IF_POSITION, &joint_pos_[i]));
+      vel_cmd_interfaces_.emplace_back(hardware_interface::CommandInterface(
+        joint_names_[i], hardware_interface::HW_IF_VELOCITY, &joint_vel_[i]));
+      acc_cmd_interfaces_.emplace_back(hardware_interface::CommandInterface(
+        joint_names_[i], hardware_interface::HW_IF_ACCELERATION, &joint_acc_[i]));
+      eff_cmd_interfaces_.emplace_back(hardware_interface::CommandInterface(
+        joint_names_[i], hardware_interface::HW_IF_EFFORT, &joint_eff_[i]));
+
+      pos_state_interfaces_.emplace_back(hardware_interface::StateInterface(
+        joint_names_[i], hardware_interface::HW_IF_POSITION,
+        separate_cmd_and_state_values ? &joint_state_pos_[i] : &joint_pos_[i]));
+      vel_state_interfaces_.emplace_back(hardware_interface::StateInterface(
+        joint_names_[i], hardware_interface::HW_IF_VELOCITY,
+        separate_cmd_and_state_values ? &joint_state_vel_[i] : &joint_vel_[i]));
+      acc_state_interfaces_.emplace_back(hardware_interface::StateInterface(
+        joint_names_[i], hardware_interface::HW_IF_ACCELERATION,
+        separate_cmd_and_state_values ? &joint_state_acc_[i] : &joint_acc_[i]));
+      
+      // Add to export lists and set initial values
+      cmd_interfaces.emplace_back(pos_cmd_interfaces_.back());
+      cmd_interfaces.back().set_value(initial_pos_joints[i]);
+      cmd_interfaces.emplace_back(vel_cmd_interfaces_.back());
+      cmd_interfaces.back().set_value(initial_vel_joints[i]);
+      cmd_interfaces.emplace_back(acc_cmd_interfaces_.back());
+      cmd_interfaces.back().set_value(initial_acc_joints[i]);
+      cmd_interfaces.emplace_back(eff_cmd_interfaces_.back());
+      cmd_interfaces.back().set_value(initial_eff_joints[i]);
+      if (separate_cmd_and_state_values)
+      {
+        joint_state_pos_[i] = INITIAL_POS_JOINTS[i];
+        joint_state_vel_[i] = INITIAL_VEL_JOINTS[i];
+        joint_state_acc_[i] = INITIAL_ACC_JOINTS[i];
+      }
+      state_interfaces.emplace_back(pos_state_interfaces_.back());
+      state_interfaces.emplace_back(vel_state_interfaces_.back());
+      state_interfaces.emplace_back(acc_state_interfaces_.back());
+    }
+
+    controller_->assign_interfaces(std::move(cmd_interfaces), std::move(state_interfaces));
+    return controller_->get_node()->activate();
+  }
+
+  void InitializeConfigureActivatePFC(
+    rclcpp::Executor & ex,
+    const std::vector<rclcpp::Parameter> & parameters = {},
+    bool does_angle_wraparound = false,
+    bool do_cmd_and_state_values_differ = false,
+    const std::vector<double> initial_pos_joints = INITIAL_POS_JOINTS,
+    const std::vector<double> initial_vel_joints = INITIAL_VEL_JOINTS,
+    const std::vector<double> initial_acc_joints = INITIAL_ACC_JOINTS,
+    const std::vector<double> initial_eff_joints = INITIAL_EFF_JOINTS)
+  {
+    InitializePFC(ex, parameters);
+
+    controller_->trigger_declare_parameters();
+    for (size_t i = 0; i < joint_names_.size(); ++i)
+    {
+      const std::string prefix = "gains." + joint_names_[i];
+      const rclcpp::Parameter angle_wraparound(
+        prefix + ".angle_wraparound", does_angle_wraparound);
+      controller_->get_node()->set_parameters({angle_wraparound});
+    }
+
+    ConfigurePFC();
+    ActivatePFC(
+      do_cmd_and_state_values_differ,
+      initial_pos_joints, 
+      initial_vel_joints, 
+      initial_acc_joints, 
+      initial_eff_joints);
+  }
+
+  rclcpp_lifecycle::State DeactivatePFC(){
+    return controller_->get_node()->deactivate();
+  }
+  rclcpp_lifecycle::State CleanUpPFC(){
+    return controller_->get_node()->cleanup();
+  }
+
+  /**
+   * @brief a wrapper for update() method of JTC, running synchronously with the clock
+   * @param wait_time - the time span for updating the controller
+   * @param update_rate - the rate at which the controller is updated
+   *
+   * @note use the faster updateControllerAsync() if no subscriptions etc.
+   * have to be used from the waitSet/executor
+   */
+  void UpdatePFC(
+    rclcpp::Duration wait_time = rclcpp::Duration::from_seconds(0.2),
+    const rclcpp::Duration update_rate = rclcpp::Duration::from_seconds(0.01))
+  {
+    auto clock = rclcpp::Clock(RCL_STEADY_TIME);
+    const auto start_time = clock.now();
+    const auto end_time = start_time + wait_time;
+    auto previous_time = start_time;
+
+    while (clock.now() <= end_time)
+    {
+      auto now = clock.now();
+      controller_->update(now, now - previous_time);
+      previous_time = now;
+      std::this_thread::sleep_for(update_rate.to_chrono<std::chrono::milliseconds>());
+    }
+  }
+
+  /**
+   * @brief a wrapper for update() method of JTC, running asynchronously from the clock
+   * @return the time at which the update finished
+   * @param wait_time - the time span for updating the controller
+   * @param start_time - the time at which the update should start
+   * @param update_rate - the rate at which the controller is updated
+   *
+   * @note this is faster than updateController() and can be used if no subscriptions etc.
+   * have to be used from the waitSet/executor
+   */
+  rclcpp::Time UpdateAsyncPFC(
+    rclcpp::Duration wait_time = rclcpp::Duration::from_seconds(0.2),
+    rclcpp::Time start_time = rclcpp::Time(0, 0, RCL_STEADY_TIME),
+    const rclcpp::Duration update_rate = rclcpp::Duration::from_seconds(0.01))
+  {
+    if (start_time == rclcpp::Time(0, 0, RCL_STEADY_TIME))
+    {
+      start_time = rclcpp::Clock(RCL_STEADY_TIME).now();
+    }
+    const auto end_time = start_time + wait_time;
+    auto time_counter = start_time;
+    while (time_counter <= end_time)
+    {
+      controller_->update(time_counter, update_rate);
+      time_counter += update_rate;
+    }
+    return end_time;
+  }
+
+  /**
+   *  @brief Publish trajectory msgs with multiple points
+   *  @param delay_btwn_points delay between each points
+   *  @param start_time timestamp for msg header
+   *  @param points_positions vector of trajectory-positions. One point per controlled joint
+   *  @param points_velocities vector of trajectory-velocities. One point per controlled joint
+   *  @param joint_names names of joints, if empty, will use joint_names_ up to the number of provided
+   *  points
+   */
+  void PublishToJointTrajectory(
+    const builtin_interfaces::msg::Duration & delay_btwn_points,
+    const std::vector<std::vector<double>> & points_positions,
+    const std::vector<std::vector<double>> & points_velocities = {},
+    const std::vector<std::string> & joint_names = {},
+    rclcpp::Time start_time = rclcpp::Time())
+  {
+    int wait_count = 0;
+    const auto topic = trajectory_publisher_->get_topic_name();
+    while (node_->count_subscribers(topic) == 0)
+    {
+      if (wait_count >= 5)
+      {
+        auto error_msg = std::string("publishing to ") + topic + " but no node subscribes to it";
+        throw std::runtime_error(error_msg);
+      }
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      ++wait_count;
+    }
+
+    trajectory_msgs::msg::JointTrajectory traj_msg;
+    if (joint_names.empty())
+    {
+      traj_msg.joint_names = {
+        joint_names_.begin(), joint_names_.begin() + points_positions[0].size()};
+    }
+    else
+    {
+      traj_msg.joint_names = joint_names;
+    }
+    traj_msg.header.stamp = start_time;
+    traj_msg.points.resize(points_positions.size());
+
+    builtin_interfaces::msg::Duration duration_msg;
+    duration_msg.sec = delay_btwn_points.sec;
+    duration_msg.nanosec = delay_btwn_points.nanosec;
+    rclcpp::Duration duration(duration_msg);
+    rclcpp::Duration duration_total(duration_msg);
+
+    for (size_t index = 0; index < points_positions.size(); ++index)
+    {
+      traj_msg.points[index].time_from_start = duration_total;
+
+      traj_msg.points[index].positions.resize(points_positions[index].size());
+      for (size_t j = 0; j < points_positions[index].size(); ++j)
+      {
+        traj_msg.points[index].positions[j] = points_positions[index][j];
+      }
+      duration_total = duration_total + duration;
+    }
+
+    for (size_t index = 0; index < points_velocities.size(); ++index)
+    {
+      traj_msg.points[index].velocities.resize(points_velocities[index].size());
+      for (size_t j = 0; j < points_velocities[index].size(); ++j)
+      {
+        traj_msg.points[index].velocities[j] = points_velocities[index][j];
+      }
+    }
+
+    trajectory_publisher_->publish(traj_msg);
+  }
 
   void getState(); //returns state msg
 
@@ -176,7 +440,7 @@ public:
   void compareJoints(){} // compare names of itnerfaces and joints
 
   // examine necessity
-  void TestStatePublishRateTarget(); //what
+  void TestStatePublishRateTarget(){} //what
 
 
   std::shared_ptr<TestablePathFollowingController> controller_;
@@ -202,8 +466,10 @@ public:
   std::vector<hardware_interface::StateInterface> vel_state_interfaces_;
   std::vector<hardware_interface::StateInterface> acc_state_interfaces_;
 
-  // review necessity
+  rclcpp::Publisher<JointTrajectory>::SharedPtr trajectory_publisher_;
+  rclcpp::Node::SharedPtr node_;
 
+  // review necessity
   rclcpp::Subscription<control_msgs::msg::JointTrajectoryControllerState>::SharedPtr
     state_subscriber_;
   mutable std::mutex state_mutex_;
@@ -225,10 +491,6 @@ class GoalFromTopicFixturePFC : public UpdateLoopFixturePFC
 {
 public:
   virtual void SetUp(){ 
-    UpdateLoopFixturePFC::SetUp(); 
-    node_ = std::make_shared<rclcpp::Node>("trajectory_publisher_");
-    trajectory_publisher_ = node_->create_publisher<trajectory_msgs::msg::JointTrajectory>(
-      controller_name_ + "/joint_trajectory", rclcpp::SystemDefaultsQoS());
   }
   virtual void TearDown(){ FixturePFC::TearDown(); }
   
@@ -237,8 +499,7 @@ public:
   void subscribeToControllerState();
   void publishToControllerState();
 
-  rclcpp::Publisher<JointTrajectory>::SharedPtr trajectory_publisher_;
-  rclcpp::Node::SharedPtr node_;
+  
 };
 
 class GoalFromActionFixturePFC : public UpdateLoopFixturePFC
